8222e846e0806b9fe52b5d538c131d40cf4ba69c
vishnu <vishnu2571@gmail.com>
2023-06-25 11:00:18 -0500

    task

--- src/test/java/AddControllerTest.java
+++ src/test/java/AddControllerTest.java
@@ -13,6 +13,7 @@ public class AddControllerTest {
                 "  \"number2\": 20\n" +
                 "}";
 
+
         // Make the POST request and validate the response
         given()
                 .contentType(ContentType.JSON)
@@ -23,5 +24,41 @@ public class AddControllerTest {
                 .statusCode(200)
                 .body(equalTo("The result is: 50"));
     }
+
+    @Test
+    public void testAddTwoNulls() {
+        String requestBody = "{\n" +
+                "  \"number1\": "+null+",\n" +
+                "  \"number2\": "+null+"\n" +
+                "}";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("http://localhost:8081/integers/add")
+                .then()
+                .statusCode(200)
+                .body(equalTo("The result is: "+0));
+    }
+    @Test
+    public void testInvalidBody() {
+
+
+        /*String requestBody = "{\n" +
+                "  \"number1\": 10,\n" +
+                "  \"number2\": 20\n" +
+                "}";*/
+        String requestBody = "";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("http://localhost:8081/integers/add")
+                .then()
+                .statusCode(400);
+
+}
 
 }
